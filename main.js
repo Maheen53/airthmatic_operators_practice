6379157062ed6143ba1a0d64243c60155d035ef5
“[firstname <Maheen53@users.noreply.github.com>
2024-04-21 15:39:09 -0700

    airthmatic operators practice

---
+++ main.js
@@ -0,0 +1,16 @@
+"use strict";
+const num1 = 10;
+const num2 = 20;
+const num3 = 90;
+// console.log(num1 + num2 + num3)     //addition
+const result = num1 + num2 + num3;
+console.log(result); // addition
+//          10   +   20  + 90 
+console.log(num1 * num2); // multiplication
+//         10 * 
+console.log(num1 - num2); //subtraction
+//         10 - 20
+console.log(num1 / num2); //division
+//            10 / 20
+console.log(3 % 22); //modulus
+console.log(21 % 2);
